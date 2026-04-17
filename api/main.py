a343558d9f9d2a2d8a7d7bfad6333a7d1d6e0f22
jairo <john.canaveral921@pascualbravo.edu.co>
2026-04-17 00:07:14 -0500

    deploy fastapi render

--- api/main.py
+++ api/main.py
@@ -1,15 +1,13 @@
+import os
 from fastapi import FastAPI
 import psycopg
 
 app = FastAPI()
 
+DATABASE_URL = "postgresql://carteradb_qe9z_user:AwsuVYcjmkAG71BMiILOHqjRM8MIcARc@dpg-d7grhq3eo5us739ja110-a.oregon-postgres.render.com/carteradb_qe9z"
+
 def get_connection():
-    return psycopg.connect(
-        host="localhost",
-        dbname="cartera_db",
-        user="postgres",
-        password="postgres123"
-    )
+    return psycopg.connect(DATABASE_URL)
 
 @app.get("/")
 def home():
@@ -19,58 +17,45 @@ def home():
 def obtener_cartera():
     conn = get_connection()
     cur = conn.cursor()
-
     cur.execute("SELECT * FROM cartera LIMIT 100;")
-
     columnas = [desc[0] for desc in cur.description]
     resultado = [dict(zip(columnas, fila)) for fila in cur.fetchall()]
-
     cur.close()
     conn.close()
-
     return resultado
 
 @app.get("/cartera/tipo/{tipo}")
 def cartera_por_tipo(tipo: str):
     conn = get_connection()
     cur = conn.cursor()
-
     cur.execute("""
-    SELECT * FROM cartera
-    WHERE unaccent(LOWER(tipo_cartera)) = unaccent(LOWER(%s))
-""", (tipo,))
-
+        SELECT * FROM cartera
+        WHERE unaccent(LOWER(tipo_cartera)) = unaccent(LOWER(%s))
+    """, (tipo,))
     columnas = [desc[0] for desc in cur.description]
     resultado = [dict(zip(columnas, fila)) for fila in cur.fetchall()]
-
     cur.close()
     conn.close()
-
     return resultado
 
 @app.get("/cartera/fecha/{fecha}")
 def cartera_por_fecha(fecha: str):
     conn = get_connection()
     cur = conn.cursor()
-
     cur.execute("""
         SELECT * FROM cartera
         WHERE fecha_corte = %s
     """, (fecha,))
-
     columnas = [desc[0] for desc in cur.description]
     resultado = [dict(zip(columnas, fila)) for fila in cur.fetchall()]
-
     cur.close()
     conn.close()
-
     return resultado
 
 @app.get("/metricas")
 def metricas():
     conn = get_connection()
     cur = conn.cursor()
-
     cur.execute("""
         SELECT 
             SUM(saldo_cartera) AS total_cartera,
@@ -78,12 +63,9 @@ def metricas():
             SUM(clientes_mora) AS total_clientes_mora
         FROM cartera
     """)
-
     fila = cur.fetchone()
-
     cur.close()
     conn.close()
-
     return {
         "total_cartera": fila[0],
         "total_vencida": fila[1],
